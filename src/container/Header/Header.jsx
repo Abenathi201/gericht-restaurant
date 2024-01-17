e7d64515d3ba7c9976542803b9805e9ca70a1aba
Abenathi201 <abenathi.sindaphi201@gmail.com>
2024-01-17 16:04:57 +0200

    initial push

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__section" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading />
+    </div>
+
+    <div className="app__wrapper_img"></div>
+  </div>
+);
+
+export default Header;
